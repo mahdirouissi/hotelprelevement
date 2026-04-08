363b72382090ac414c60c4e67e6f3d9e108abec5
mahdi rouissi <mehdi.rouissi@addinn.com>
2026-04-09 00:47:29 +0100

    chore: add EF migration for QuantiteDonne column

---
+++ MyApi/Migrations/20260408234634_UpdateItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "QuantiteDonne",
+                table: "RequestItems",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantiteDonne",
+                table: "RequestItems");
+        }
+    }
+}
